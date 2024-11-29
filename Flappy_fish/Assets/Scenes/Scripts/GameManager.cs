ba32ab522386419984a8876073f4bee32cb0980b
SoloEkaterina <katia-sol@mail.ru>
2024-11-28 22:20:11 -0700

    TMP fix

--- Flappy_fish/Assets/Scenes/Scripts/GameManager.cs
+++ Flappy_fish/Assets/Scenes/Scripts/GameManager.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEditor.SceneManagement;
+using TMPro;
+using System;
+
 public class GameManager : MonoBehaviour
 {
     public GameObject startButton;
     public Player player;
 
-    public Text gameOverCountdown;
+    public TMP_Text gameOverCountdown;
     public float countTimer = 5;
 
     // Start is called before the first frame update
